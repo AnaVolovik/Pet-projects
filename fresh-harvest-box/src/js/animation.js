00e636f2f3e6d3706dfb8f5aaa62bbf1e777a06c
AnaVolovik <nastasia.volovik@gmail.com>
2024-07-16 10:05:56 +0300

    Add animation to About section

---
+++ fresh-harvest-box/src/js/animation.js
@@ -0,0 +1,11 @@
+// ANIMATION
+
+const shopNow = document.querySelector('.shop-now');
+const description = document.querySelector('.description');
+const tags = document.querySelector('.tags');
+
+window.onload = function() {
+  shopNow.classList.add('show');
+  description.classList.add('show');
+  tags.classList.add('show');
+};
--- fresh-harvest-box/src/js/app.js
+++ fresh-harvest-box/src/js/app.js
@@ -1,6 +1,7 @@
 import * as myFunctions from "./modules/functions.js";
 myFunctions.isWebp();
 
+import animation from "./animation.js";
 import burger from "./burger.js";
 import showFruits from "./showFruits.js";
 import getOrder from "./getOrder.js";
