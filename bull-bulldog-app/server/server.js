5d5aaceb73a78369a315bfa1c77d2aba472fc656
AnaVolovik <nastasia.volovik@gmail.com>
2024-07-25 17:20:51 +0300

    Add functionality to send registration data to the database
    - Implemented insertion of user registration data into the registr_data table
    - Implemented insertion of user contact data into the contacts table
    - Handled foreign key relationship between registr_data and contacts tables

--- bull-bulldog-app/server/server.js
+++ bull-bulldog-app/server/server.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const db = require('./db');
-const cors = require('cors'); // Для настройки CORS
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors()); // Разрешает запросы с других доменов
+app.use(cors());
 
 // Route to get the list of cities
-// app.get('/api/cities', async (req, res) => {
-//   try {
-//     const result = await db.query('SELECT DISTINCT city FROM contacts');
-//     res.json(result.rows.map(row => row.city));
-//   } catch (error) {
-//     console.error('Error fetching cities:', error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
-
-// Маршрут для получения списка городов
 app.get('/api/cities', (req, res) => {
   db.query('SHOW COLUMNS FROM contacts LIKE "city"', (err, results) => {
     if (err) {
@@ -30,9 +19,6 @@ app.get('/api/cities', (req, res) => {
     const cityEnum = results[0].Type;
     const cities = cityEnum.match(/'([^']+)'/g).map((city) => city.replace(/'/g, ''));
 
-    // Вывод списка городов в консоль
-    console.log('Список городов:', cities);
-    
     res.json({ cities });
   });
 });
@@ -45,9 +31,30 @@ app.post('/api/register', (req, res) => {
     return res.status(400).json({ message: 'Все поля обязательны' });
   }
   
-  // Здесь добавьте логику регистрации пользователя
+  const dateReg = new Date().toISOString().split('T')[0]; // Получаем текущую дату в формате YYYY-MM-DD
+  const role = 'user';
 
-  res.status(200).json({ user: { name, email, city, phone } });
+  // Вставка данных в таблицу registr_data
+  db.query('INSERT INTO registr_data (name_reg, email, password, date_reg, role) VALUES (?, ?, ?, ?, ?)', 
+    [name, email, password, dateReg, role], (err, results) => {
+    if (err) {
+      console.error('Ошибка выполнения запроса:', err);
+      return res.status(500).json({ message: 'Ошибка сервера' });
+    }
+
+    const registrDataId = results.insertId; // Получаем ID вставленной записи
+
+    // Вставка данных в таблицу contacts
+    db.query('INSERT INTO contacts (city, tel_num, fk_con_reg) VALUES (?, ?, ?)', 
+      [city, phone, registrDataId], (err, results) => {
+      if (err) {
+        console.error('Ошибка выполнения запроса:', err);
+        return res.status(500).json({ message: 'Ошибка сервера' });
+      }
+
+      res.status(200).json({ user: { name, email, city, phone } });
+    });
+  });
 });
 
 app.listen(port, () => {
